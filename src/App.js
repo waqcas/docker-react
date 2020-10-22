0326b56ec40bf6315b207f25b6ed321c41802c69
waqas <waqcas@gmail.com>
2020-10-22 22:37:10 +0500

    mmmm

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Huzaifa Waqas
+          Huzaifa Waqas khalique
         </p>
         <a
           className="App-link"
